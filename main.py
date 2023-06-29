04d53bed8062974ac0c2118267073059731eb8fa
Markmanus <pampadalampa@gmail.com>
2023-06-29 23:38:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Screen
+import time
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("pong")
+screen.tracer(0)
+
+#placing a pause to see the black screen
+time.sleep(1)
