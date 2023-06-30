b9cd97c10ca72ac1e7d3af287b6820371758f30a
Markmanus <pampadalampa@gmail.com>
2023-06-30 20:41:51 +0100

    0.7 added ball support

---
+++ ball.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+
+#ball class
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        #self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    #moving the ball
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        #self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(new_x, new_y)
+
+    #detecting collision with the wall
+    def bounce_y(self):
+        self.y_move *= -1
+
+    #detecting collision with the paddle
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    #resetting the ball
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 
@@ -10,16 +11,25 @@ screen.title("pong")
 screen.tracer(0)
 
 #creating the paddle
-paddle = Paddle((350, 0))
+rpaddle = Paddle((350, 0))
+lpaddle = Paddle((-350, 0))
+ball = Ball()
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
+    #moving the ball
+    ball.move()
+    time.sleep(ball.move_speed)
 
+    time.sleep(0.1)
     #moving the paddle
     screen.listen()
-    screen.onkey(paddle.go_up, "Up")
-    screen.onkey(paddle.go_down, "Down")
+    screen.onkey(rpaddle.go_up, "Up")
+    screen.onkey(rpaddle.go_down, "Down")
+    screen.onkey(lpaddle.go_up, "w")
+    screen.onkey(lpaddle.go_down, "s")
 
 
 #placing a pause to see the black screen
